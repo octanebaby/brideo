305441042969c8a8e236299082953b186475befb
Janine Yoong <janine.yoong@gmail.com>
2012-05-06 12:47:12 -0700

    added event editing

--- brideo.py
+++ brideo.py
@@ -3,6 +3,8 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from contextlib import closing
 import model
+import requests
+import datetime
 
 # configuration
 DEBUG = True
@@ -32,7 +34,39 @@ def home():
 @app.route("/event/view/<int:id>")
 def view(id):
     event = g.db.query(model.Event).get(id)
-    return render_template("event_view.html", event=event)
+    url_template = "http://api.justin.tv/api/channel/embed/%s?volume=50"
+    jtv_login = event.video_url
+    r = requests.get(url_template%jtv_login)
+   
+    return render_template("event_view.html", event=event, 
+    	embed_code=r.text)
+
+@app.route("/event/edit/<int:id>")
+def edit(id):
+    event = g.db.query(model.Event).get(id)
+    
+    return render_template("event_edit.html", event=event)
+
+@app.route("/event/save", methods=["POST"])
+def save():
+    if request.form["id"]:
+        evt_id = int(request.form["id"])
+        event = g.db.query(model.Event).get(evt_id)
+    else:
+        event = model.Event("","",datetime.datetime.utcnow(),"")
+        g.db.add(event)
+    event.title = request.form["title"] or event.title
+    #event.happens_on = request.form["date"] 
+    event.message = request.form["message"] or event.message
+    event.video_url = request.form["channel"] or event.video_url
+    g.db.commit()
+    g.db.refresh(event)
+    return redirect(url_for("view", id=event.id))
+
+@app.route("/event/new")
+def new():
+    event = model.Event("","",datetime.datetime.utcnow(),"")
+    return render_template("event_edit.html", event=event)   
 
 # End Views
 
